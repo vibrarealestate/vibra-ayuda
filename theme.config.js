6375150a0c74ea75e74f20f3f1b5021f2c51a916
JuanCamiloQHz <juancamiloqhz@gmail.com>
2022-06-23 14:02:46 -0500

    name changed only

--- theme.config.js
+++ theme.config.js
@@ -1,13 +1,11 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Vibra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
-      </span>
+      <span className="mr-2 font-extrabold hidden md:inline">Vibra</span>
+      <span className="text-gray-600 font-normal hidden md:inline">Ayuda</span>
     </>
   ),
   head: (
@@ -16,15 +14,24 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta
+        name="description"
+        content="Vibra: Hacemos fácil encontrar el hogar de tus sueños"
+      />
+      <meta
+        name="og:description"
+        content="Vibra: Hacemos fácil encontrar el hogar de tus sueños"
+      />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta
+        name="og:title"
+        content="Vibra: Hacemos fácil encontrar el hogar de tus sueños"
+      />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="Vibra" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +69,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>{new Date().getFullYear()} © Vibra.</>,
   unstable_faviconGlyph: '👋',
 }
